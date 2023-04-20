67d4a09b7c93fdbb070d84965da3f20a6dd3d282
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2023-04-20 09:58:36 -0300

    Add files via upload

---
+++ src/main/java/com/ufes/cobrancaestacionamento/ICalculoTarifa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ufes.cobrancaestacionamento;
+
+import com.ufes.cobrancaestacionamento.model.Vaga;
+
+/**
+ *
+ * @author guiro
+ */
+public interface ICalculoTarifa {
+    public double calcular(Vaga vaga);
+}
